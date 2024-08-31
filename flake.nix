3be3aa001c2641a0aa4bd4cf66872ef954a461b4
weriomat <engel@weriomat.com>
2024-08-31 14:59:00 +0200

    chore: bump flake

--- flake.nix
+++ flake.nix
@@ -16,20 +16,41 @@
         };
       in {
         packages = {
+          c = pkgs.runCommand "c" {} ''
+            mkdir $out
+            for WALLPAPER in $(find ./wallpapers -type f)
+            do
+              ${pkgs.lutgen}/bin/lutgen apply $WALLPAPER -o $out/$(basename $WALLPAPER) -p catppuccin-mocha
+            done
+          '';
+          b = let
+            path = "./wallpapers";
+          in
+            pkgs.runCommand "prism" {} ''
+              mkdir -p $out/lut_wallpapers
+              find ${path} -type f -exec ${pkgs.lutgen}/bin/lutgen apply {} -p catppuccin-mocha -o $out/lut_wallpapers/{} \;
+            '';
           a = pkgs.stdenv.mkDerivation {
             pname = "weriomat wallpapers";
             version = "0.0.1";
 
+            src = builtins.path {
+              name = "wallpapers";
+              path = ./wallpapers;
+            };
+
             buildInputs = with pkgs; [lutgen findutils coreutils];
+
+            buildPhase = ''
+              mkdir -p $out/lut_wallpapers
+              find . -type f -exec ${pkgs.lutgen}/bin/lutgen apply {} -p catppuccin-mocha -o $out/lut_wallpapers/{} \;
+            '';
 
             installPhase = ''
               runHook preInstall
 
-              mkdir -p "$out"/wallpapers
-              for WALLPAPER in $(find ./wallpapers -type f)
-              do
-                ${pkgs.lutgen}/bin/lutgen apply $WALLPAPER -o $out/$(basename $WALLPAPER) -p catppuccin-mocha
-              done
+              mv $out/lut_wallpapers/* $out
+              rm -r $out/lut_wallpapers
 
               runHook postInstall
             '';
@@ -37,15 +58,13 @@
           default = pkgs.writeShellApplication {
             name = "lut";
             runtimeInputs = with pkgs; [lutgen findutils coreutils];
-            text =
-              #zsh
-              ''
-                rm -r ./lut_wallpapers
-                mkdir -p ./lut_wallpapers
-                find ./wallpapers -type f -exec ${pkgs.lutgen}/bin/lutgen apply {} -p catppuccin-mocha -o ./lut_wallpapers/{} \;
-                mv ./lut_wallpapers/wallpapers/* ./lut_wallpapers
-                rm -r ./lut_wallpapers/wallpapers
-              '';
+            text = ''
+              rm -r ./lut_wallpapers
+              mkdir -p ./lut_wallpapers
+              find ./wallpapers -type f -exec ${pkgs.lutgen}/bin/lutgen apply {} -p catppuccin-mocha -o ./lut_wallpapers/{} \;
+              mv ./lut_wallpapers/wallpapers/* ./lut_wallpapers
+              rm -r ./lut_wallpapers/wallpapers
+            '';
           };
           # pkgs.runCommand "prism" {} ''
           #   mkdir $out/lut_wallpapers
